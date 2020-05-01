f5166e97f5856b09ced9c680a6c3b827bf48a2a8
wndenis <winda.wzse@gmail.com>
2020-05-01 23:35:26 +0300

    Initial empty configured upload

---
+++ Assets/Editor/EnhanceCharacterJointEditor.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(EnhanceCharacterJoint))]
+class EnhanceCharacterJointEditor : Editor
+{
+    void OnSceneGUI () {
+       
+        //Debug.Log(this.target.GetType());
+        EnhanceCharacterJoint script = (EnhanceCharacterJoint) this.target;
+        if (script != null)
+        {
+            CharacterJoint joint = (CharacterJoint) script.gameObject.GetComponent(typeof(CharacterJoint));
+           
+            Vector3 twistAxis = joint.transform.TransformDirection(joint.axis).normalized;
+            Vector3 swing1Axis = joint.transform.TransformDirection(joint.swingAxis).normalized;
+            Vector3 swing2Axis = Vector3.Cross(twistAxis, swing1Axis);
+
+            var position = joint.transform.position;
+            float size = HandleUtility.GetHandleSize(position);
+           
+            Handles.color = new Color(1,0.5f,0) ; // orange == twist, direction = swing1, rotates about twist axis
+            Handles.DrawWireArc(position, twistAxis, swing1Axis, joint.highTwistLimit.limit, size);
+            Handles.DrawWireArc(position, twistAxis, swing1Axis, joint.lowTwistLimit.limit, size);
+            Handles.DrawLine(position, position + Quaternion.AngleAxis(joint.highTwistLimit.limit, twistAxis) * swing1Axis * size);
+            Handles.DrawLine(position, position + Quaternion.AngleAxis(joint.lowTwistLimit.limit, twistAxis) * swing1Axis * size);
+           
+            Handles.color = new Color(0,0.3f,0) ; // green=swing1, direction=twistAxis rotates about swing1Axis
+            Handles.DrawWireArc(position, swing1Axis, twistAxis, joint.swing1Limit.limit, size);
+            Handles.DrawWireArc(position, swing1Axis, twistAxis, -joint.swing1Limit.limit, size);          
+            Handles.DrawLine(position, position + Quaternion.AngleAxis(joint.swing1Limit.limit, swing1Axis) * twistAxis * size);
+            Handles.DrawLine(position, position + Quaternion.AngleAxis(-joint.swing1Limit.limit, swing1Axis) * twistAxis * size);
+           
+            Handles.color = new Color(0,0,0.5f); // blue=swing2, direction=twistAxis, rotates about swing2 axis
+            Handles.DrawWireArc(position, swing2Axis, twistAxis, joint.swing2Limit.limit, size);
+            Handles.DrawWireArc(position, swing2Axis, twistAxis, -joint.swing2Limit.limit, size);          
+            Handles.DrawLine(position, position + Quaternion.AngleAxis(joint.swing2Limit.limit, swing2Axis) * twistAxis * size);
+            Handles.DrawLine(position, position + Quaternion.AngleAxis(-joint.swing2Limit.limit, swing2Axis) * twistAxis * size);              
+        }
+    }
+}
---
+++ Assets/SteamVR_Resources/CustomControllerLol.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Resources.Scripts;
+using RootMotion.Demos;
+using UnityEngine;
+
+public class CustomControllerLol : UserControlThirdPerson
+{ 
+	/// <summary>
+	/// User input for an AI controlled character controller.
+	/// </summary>
+	// public MoveTarget moveTarget;
+	// public Transform moveTarget;
+	// public float stoppingDistance = 0.5f;
+	// public float stoppingThreshold = 1.5f;
+	// public float brakingDistance = 5f;
+	public Navigator navigator;
+
+	protected override void Start()
+	{
+		base.Start();
+		navigator.Initiate(transform);
+	}
+
+    protected override void Update () {
+	       //
+        // float moveSpeed = 1f;
+        // if (moveTarget == null) //todo fix it
+	       //  return;
+        // // If using Unity Navigation
+        // if (navigator.activeTargetSeeking)
+        // {
+        //     navigator.Update(moveTarget.position);
+        //     var speed = moveSpeed;
+        //     if (navigator.lastCorner)
+        //     {
+        //         if (navigator.Distance < stoppingDistance + 1.5f)
+	       //          speed = 0;
+        //         else{
+	       //          var brakingForStopping = brakingDistance + stoppingDistance;
+	       //          var deltaDist = navigator.Distance - brakingForStopping;
+	       //          // Торможение
+	       //          if (deltaDist <= 0)
+		      //           speed = Mathf.Lerp(moveSpeed, 0.0f, -deltaDist / brakingForStopping);
+        //         }
+        //     }
+        //     
+        //     state.move = navigator.normalizedDeltaPosition * speed;
+        // }
+        // // No navigation, just move straight to the target
+        // else
+        // {
+        //     Vector3 direction = moveTarget.position - transform.position;
+        //     float distance = direction.magnitude;
+        //
+        //     Vector3 normal = transform.up;
+        //     Vector3.OrthoNormalize(ref normal, ref direction);
+        //
+        //     float sD = state.move != Vector3.zero ? stoppingDistance : stoppingDistance * stoppingThreshold;
+        //
+        //     state.move = distance > sD ? direction * moveSpeed : Vector3.zero;
+        //     state.lookPos = moveTarget.position;
+        // }
+	}
+
+    // Visualize the navigator
+    void OnDrawGizmos()
+    {
+        if (navigator.activeTargetSeeking) navigator.Visualize();
+    }
+}
