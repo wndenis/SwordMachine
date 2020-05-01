e80ce48584927e3c0ad2363db72778fb4d4baf5e
wndenis <winda.wzse@gmail.com>
2020-05-01 23:39:27 +0300

    Add 3rd party assets

---
+++ Assets/Effects/Misc Effects/Scripts/SpawnEffect.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SpawnEffect : MonoBehaviour {
+
+    public float spawnEffectTime = 2;
+    public float pause = 1;
+    public AnimationCurve fadeIn;
+
+    ParticleSystem ps;
+    float timer = 0;
+    Renderer _renderer;
+
+    int shaderProperty;
+
+	void Start ()
+    {
+        shaderProperty = Shader.PropertyToID("_cutoff");
+        _renderer = GetComponent<Renderer>();
+        ps = GetComponentInChildren <ParticleSystem>();
+
+        var main = ps.main;
+        main.duration = spawnEffectTime;
+
+        ps.Play();
+
+    }
+	
+	void Update ()
+    {
+        if (timer < spawnEffectTime + pause)
+        {
+            timer += Time.deltaTime;
+        }
+        else
+        {
+            ps.Play();
+            timer = 0;
+        }
+
+        foreach (var mat in _renderer.materials)
+        {
+            mat.SetFloat(shaderProperty, fadeIn.Evaluate( Mathf.InverseLerp(0, spawnEffectTime, timer)));
+        }
+
+        
+    }
+}
